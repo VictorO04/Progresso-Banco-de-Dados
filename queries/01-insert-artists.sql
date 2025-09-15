93b991623cb96c2dc3c0744710cf67f584acf344
Victor Oliveira <victorcriri313@gmail.com>
2025-09-15 15:58:53 -0300

    feat: cria as queries de select

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,3 +10,21 @@ CREATE TABLE artistas (
     musica_famosa VARCHAR(50),
     ativo BOOLEAN
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+('Adele', 'Solo', 1, 'Pop', 2006, 'Reino Unido', 'Rolling in the Deep', TRUE),
+('21 Pilots', 'Banda', 2, 'Rock Alternativo', 2009, 'Estados Unidos', 'Stressed Out', TRUE)
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+('Coldplay', 'Banda', 4, 'Rock Alternativo', 1996, 'Reino Unido', 'Viva la Vida', TRUE),
+('Beyoncé', 'Solo', 1, 'R&B/Pop', 2003, 'Estados Unidos', 'Single Ladies', TRUE),
+('Arctic Monkeys', 'Banda', 4, 'Indie Rock', 2002, 'Reino Unido', 'Do I Wanna Know?', TRUE),
+('Shakira', 'Solo', 1, 'Pop Latino', 1995, 'Colômbia', 'Hips Don’t Lie', TRUE),
+('Imagine Dragons', 'Banda', 4, 'Pop Rock', 2008, 'Estados Unidos', 'Radioactive', TRUE),
+('Elton John', 'Solo', 1, 'Pop/Rock', 1969, 'Reino Unido', 'Rocket Man', TRUE),
+('Rihanna', 'Solo', 1, 'Pop/R&B', 2005, 'Barbados', 'Umbrella', TRUE),
+('Linkin Park', 'Banda', 6, 'Nu Metal/Rock', 1996, 'Estados Unidos', 'In the End', FALSE),
+('The Weeknd', 'Solo', 1, 'R&B/Pop', 2011, 'Canadá', 'Blinding Lights', TRUE),
+('Daft Punk', 'Dupla', 2, 'Eletrônica', 1993, 'França', 'Get Lucky', FALSE);
