2f5a46cb41b1a8126e6d224215ff670ca9e4027f
Victor Oliveira <victorcriri313@gmail.com>
2025-09-22 08:59:25 -0300

    comando iniciais de update

---
+++ queries/03-update.sql
@@ -0,0 +1,7 @@
+UPDATE usuarios SET username = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+UPDATE usuarios SET email = 'master@gmail.com'
+WHERE email = 'master@email.com';
